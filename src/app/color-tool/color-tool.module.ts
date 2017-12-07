2930700ea65cae48a511ad94c7ef539e5e460fc8
Brandon Tweed <brandon.tweed@gmail.com>
2017-12-07 10:11:30 -0500

    Initial drop of demo project

---
+++ src/app/color-tool/color-tool.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColorHomeComponent } from './color-home/color-home.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ColorHomeComponent]
+})
+export class ColorToolModule { }
